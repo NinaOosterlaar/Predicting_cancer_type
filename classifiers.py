b5e50ca4c1b0152278081d644e75ae4d2982ead9
NinaOosterlaar <ninaoosterlaar@gmail.com>
2024-03-24 14:48:11 +0100

    yo

--- classifiers.py
+++ classifiers.py
@@ -16,39 +16,48 @@ LIVER = False
 def random_forest(parameters):
     X, y = choose_cancers(BREAST, LUNG, TESTICULAR, MELANOMA, LIVER)
     clf = RandomForestClassifier(**parameters)
-    scores = cross_val_score(clf, X, y, cv=5)
-    print("Random Forest Classifier")
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    scores = evaluate(clf, X, y)
     
 
 def logistic_regression(parameters):
     X, y = choose_cancers(BREAST, LUNG, TESTICULAR, MELANOMA, LIVER)
     clf = LogisticRegression(max_iter=5000, **parameters)
-    scores = cross_val_score(clf, X, y, cv=5)
-    print("Logistic Regression")
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    scores = evaluate(clf, X, y)
     
 
 def support_vector_machine(parameters):
     X, y = choose_cancers(BREAST, LUNG, TESTICULAR, MELANOMA, LIVER)
     clf = SVC(**parameters)
-    scores = cross_val_score(clf, X, y, cv=5)
-    print("Support Vector Machine")
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    scores = evaluate(clf, X, y)
     
     
 def linear_discriminant_analysis(parameters):
     X, y = choose_cancers(BREAST, LUNG, TESTICULAR, MELANOMA, LIVER)
     clf = LinearDiscriminantAnalysis(**parameters)
-    scores = cross_val_score(clf, X, y, cv=5)
-    print("Linear Discriminant Analysis")
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    scores = evaluate(clf, X, y)
     
     
 def get_parameters(filename: str):
     parameters = retrieve_parameters(filename, BREAST, LUNG, MELANOMA)
     parameters = preprocess_parameters(parameters)
     return parameters
+
+
+def evaluate(clf, X, y):
+    accuracy = cross_val_score(clf, X, y, cv=5)
+    roc_auc = cross_val_score(clf, X, y, cv=5, scoring='roc_auc')
+    precision = cross_val_score(clf, X, y, cv=5, scoring='precision')
+    recall = cross_val_score(clf, X, y, cv=5, scoring='recall')
+    specificity = cross_val_score(clf, X, y, cv=5, scoring='specificity')
+    f1 = cross_val_score(clf, X, y, cv=5, scoring='f1')
+    scores = {"accuracy": accuracy, "roc_auc": roc_auc, "precision": precision, "recall": recall, "specificity": specificity, "f1": f1}
+    print(scores)
+    return scores
+
+
+def save_scores(scores, filename, model):
+    with open(filename, "a") as f:
+        f.write(model + "\n")
 
 
 if __name__ == "__main__":
